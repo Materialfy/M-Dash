5139ca21926e97895ef5de50d8b49faec27e21b7
Clint <clintoxen@gmail.com>
2021-06-16 15:27:53 -0400

    readded comments to drawer.js but not fixed yet

--- src/store/modules/drawer.js
+++ src/store/modules/drawer.js
@@ -1,6 +1,6 @@
 
 
-//need to create some gettersand replace some code in the drawer componentfiles with this
+//need to create some getters and replace some code in the drawer component files with this
 const getters = {
 
 }
@@ -13,10 +13,14 @@ const actions = {
 //mutations
 const mutations = {
   //these are nested arrow functions aka curried functions
+  // this passes in the state to thats gonna be changed and the payload with the data to change it
+  // example: set = color => return (state, newcolor) => return (state[color] = newcolor)
   set = property => (state, payload) => (state[property] = payload),
   toggle = property => state => (state[property] = !state[property]),
 
   // calls just the first part of the nested functions, but the second part still happens
+  // they are called in toolbar.vue, filter.vue and drawer.vue
+  //they change the state of what they are setting(image,color etc)
   setDrawer: set('drawer'),
   setImage: set('image'),
   setColor: set('color'),
