ab38c79fd0e77dd072ed46449f3c51c5563690e5
Clint <clintoxen@gmail.com>
2021-06-20 20:14:23 -0400

    updated auto global components index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,17 +2,39 @@ import Vue from 'vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
+/* This file automatically imports the base components so it can be used anywhere without implicitly importing
+Aka Automatic Global Registration
+*/
 // eslint-disable-next-line no-undef
 const requireComponent = require.context(
-  '@/components', true, /\.vue$/
+  // The relative path of the components folder
+  './components',
+  // Whether or not to look in subfolders
+  false,
+  // The regular expression used to match base component filenames
+  /Base[A-Z]\w+\.(vue|js)$/
 )
-
 requireComponent.keys().forEach(fileName => {
+  // Get component config
   const componentConfig = requireComponent(fileName)
-
+  // Get PascalCase name of component
   const componentName = upperFirst(
-    camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, ''))
+    camelCase(
+      // Gets the file name regardless of folder depth
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
   )
 
-  Vue.component(componentName, componentConfig.default || componentConfig)
+  // Register component globally
+  Vue.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  )
 })
+
