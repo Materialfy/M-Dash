5aa4d70eefe3817e76975b92b476b4211d03f2f8
Clint <clintoxen@gmail.com>
2021-06-25 22:46:28 -0400

    refer to previous

--- src/components/core/Sidedrawer.vue
+++ src/components/core/Sidedrawer.vue
@@ -20,13 +20,15 @@
           </v-list-item-icon>
           <v-list-item-title>Home</v-list-item-title>
         </v-list-item>
-
+        
+        <router-link to="user-profile">
         <v-list-item>
           <v-list-item-icon>
             <v-icon>mdi-account</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Account</v-list-item-title>
+          <v-list-item-title>User</v-list-item-title>
         </v-list-item>
+        </router-link>
       </v-list-item-group>
     </v-list>
   </v-navigation-drawer>
--- src/store/state.js
+++ src/store/state.js
@@ -4,5 +4,27 @@ export default {
   authStatus: '',
   token: localStorage.getItem('token') || '',
   user: {},
-  tableList: []
+  tableList: [],
+  notifications: [
+    {
+      header: '',
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      title: 'Brunch this weekend?', 
+      subtitle: `<span class="font-weight-bold">Ali Connors</span> &mdash; 
+      I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+      divider: true, inset: true 
+    },
+    {
+      header: '',
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      title: 'Brunch this weekend?', 
+      subtitle: `<span class="font-weight-bold">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`
+    },
+    {
+      header: '',
+      avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      title: 'Brunch this weekend?', 
+      subtitle: `<span class="font-weight-bold">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`
+    },
+  ],
 }
