2a79f237b28d716da55f68a323939e4777e6ae81
Clint <clintoxen@gmail.com>
2021-07-01 22:08:23 -0400

    starting work on materialfy cards

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-footer id="core-footer" app absolute height="82">
+	<v-footer id="core-footer" color="primary" app absolute height="82">
 		<div class="ma-2">
 			<span v-for="link in links" :key="link.name">
 				<a :href="link.Link" class="ma-2">{{
@@ -11,7 +11,7 @@
 		<span class="font-weight-light copyright">
 			&copy;
 			{{ new Date().getFullYear() }}
-			<a href="https://twitter.com/SourBlueDream" target="_blank" class="ma-2">@SBD</a>made
+			<a href="https://twitter.com/SourBlueDream" target="_blank" class="ma-2">@SBD</a> V.Dashboard by Materialfy, crafted
 			with Vuetify
 			<v-icon color="tertiary" size="17">mdi-heart</v-icon>
 		</span>
