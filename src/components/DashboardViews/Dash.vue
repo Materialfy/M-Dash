8d7193ceacf5c3845a07c2f12c3509ec942f95ac
Clint <clintoxen@gmail.com>
2021-07-04 23:23:23 -0400

    creating example cards

--- src/components/DashboardViews/Dash.vue
+++ src/components/DashboardViews/Dash.vue
@@ -37,6 +37,20 @@
         >
           <template #crdIcon> mdi-cog </template>
           <template v-slot:crdInner>
+            <v-sheet
+              class="v-sheet--offset mx-auto"
+              color="tertiary"
+              elevation="4"
+              max-width="calc(100% - 32px)"
+            >
+              <v-sparkline
+                :labels="labels"
+                :value="value"
+                color="white"
+                line-width="2"
+                padding="16"
+              ></v-sparkline>
+            </v-sheet>
           </template>
         </MaterialfyHeaderCard>
       </v-col>
@@ -48,7 +62,7 @@
                 <div class="text-overline mb-4">
                   OVERLINE
                 </div>
-                <v-list-item-title class="text-h5 mb-1 innercardheight" >
+                <v-list-item-title class="text-h5 mb-1 " >
                   Headline 5
                 </v-list-item-title>
                 <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
@@ -71,7 +85,30 @@
 </template>
 
 <script>
-	export default {};
+	export default {
+    data: () => ({
+      labels: [
+        '12am',
+        '3am',
+        '6am',
+        '9am',
+        '12pm',
+        '3pm',
+        '6pm',
+        '9pm',
+      ],
+      value: [
+        200,
+        675,
+        410,
+        390,
+        310,
+        460,
+        250,
+        240,
+      ],
+    }),
+  };
 </script>
 
 <style></style>
