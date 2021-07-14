f193c79d2837726e1093b56b1fe719cc6d3cc0b7
Clint <clintoxen@gmail.com>
2021-07-14 19:43:16 -0400

    102

--- src/main.js
+++ src/main.js
@@ -9,11 +9,21 @@ import i18n from "@/i18n";
 import router from "@/router";
 import store from "@/store";
 import { genericApi } from './plugins/axios'
-//import NProgress from "nprogress";
 import vuetify from './plugins/vuetify'
+import * as Sentry from '@sentry/vue'
+import { Integrations } from '@sentry/tracing'
 
+Sentry.init({
+	Vue,
+	logErrors: true,
+	dsn: 'https://6c05050f794343c286b87c6ea643df31@o920341.ingest.sentry.io/5865924',
+	integrations: [new Integrations.BrowserTracing()],
 
-
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+})
 
 // makes genericApi common base instance axios the default http handler
 Vue.prototype.$http = genericApi;
