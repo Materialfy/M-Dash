b4d1ec82d784a5c50ead8c3867048e2c48488b6d
Just van den Broecke <just@justobjects.nl>
2021-12-01 16:10:45 +0100

    complete re-scaffold package.json and .eslintrc.js for issue Materialfy/M-Dash#80

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,35 @@
 /* eslint-disable no-undef */
 module.exports = {
-	root: true,
-	// this section will be used to determine which APIs are available to us
-	// (i.e are we running in a browser environment or a node.js env)
-	env: {
-		//node: true,
-		browser: true,
-	},
-	extends: [
-		// use the recommended rule set for both plain javascript and vue, this changes warning level
-		'eslint:recommended',
-		'plugin:vue/essential',
-		'plugin:vuetify/base',
-		//'prettier/vue',
-		//'plugin:prettier/recommended'
-	],
-	rules: {
-		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-		'template-curly-spacing': 'off',
-		'indent': 'off',
-		'vue/multi-word-component-names': ['error', {
-		    'ignores': ['Footer', 'Error', 'Calendar', 'Notifications', 'Settings', 'Toasts', 'apexchart', 'Dash']
-		  }]
-	},
-	parserOptions: {
-		parser: '@babel/eslint-parser',
-		'sourceType': 'module',
-		'requireConfigFile': false
-	},
+    root: true,
+    // this section will be used to determine which APIs are available to us
+    // (i.e are we running in a browser environment or a node.js env)
+    env: {
+        //node: true,
+        browser: true,
+    },
+    extends: [
+        // use the recommended rule set for both plain javascript and vue, this changes warning level
+        // All the "prettier" and "vuetify" linting give errors, possibly due to version mismatches
+        'plugin:vue/essential',
+        'eslint:recommended',
+        // 'plugin:prettier/recommended'
+        // '@vue/prettier'
+        // 'plugin:vuetify/base',
+        //'prettier/vue',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'template-curly-spacing': 'off',
+        'indent': 'off',
+        // For later versions
+        // 'vue/multi-word-component-names': ['error', {
+        //     'ignores': ['Footer', 'Error', 'Calendar', 'Notifications', 'Settings', 'Toasts', 'apexchart', 'Dash']
+        // }]
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+        'sourceType': 'module',
+        'requireConfigFile': false
+    },
 }
